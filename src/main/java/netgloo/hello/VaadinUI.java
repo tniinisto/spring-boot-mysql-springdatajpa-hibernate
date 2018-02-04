baa169d2d9b90183bd94b4acfe2ef28248b531ac
tniinisto@gmail.com <tniinsto@gmail.com>
2018-02-04 12:04:57 +0200

    Grid populated with data

--- src/main/java/netgloo/hello/VaadinUI.java
+++ src/main/java/netgloo/hello/VaadinUI.java
@@ -3,16 +3,34 @@ package netgloo.hello;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.*;
+import netgloo.models.PlayersEntity;
+import netgloo.models.PlayersEntityDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @SpringUI
 @Theme("valo")
 public class VaadinUI extends UI {
 
+    @Autowired
+    private PlayersEntityDAO players;
+
     @Override
     protected void init(VaadinRequest request) {
-        setContent(new Button("Click me", e -> Notification.show("Hello Spring+Vaadin user!")));
+        final VerticalLayout contentArea = new VerticalLayout();
+        contentArea.setMargin(false);
+
+        //setContent(new Button("Click me", e -> Notification.show("Hello Spring+Vaadin user!")));
+        contentArea.addComponent(new Button("Click me", e -> Notification.show("Hello Spring+Vaadin user!")));
+
+        Grid<PlayersEntity> grid = new Grid<>(PlayersEntity.class);
+        grid.setItems(players.findAll());
+
+        contentArea.addComponent(grid);
+
+        setContent(contentArea);
+
     }
 }
